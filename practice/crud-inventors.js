ca4b384ac79697397667f45208c8668930122f2f
pablof@tecnoshare.com <pablof@tecnoshare.com>
2022-04-07 21:41:38 -0300

    practica con crud json

---
+++ practice/crud-inventors.js
@@ -0,0 +1,7 @@
+// PERSISTIDO EN EL JSON inventors.json
+
+// TODO: getInventors()
+// TODO: getInventor(id)
+// TODO: insertInventor(inventor)
+// TODO: updateInventor(inventor)
+// TODO: deleteInventor(id)
