0f4511b3e261c348c86b490370b7945fb04a7787
pablof@tecnoshare.com <pablof@tecnoshare.com>
2022-05-05 22:30:01 -0300

    server web

--- demos/servers/1-serverExpress.js
+++ demos/servers/1-serverExpress.js
@@ -5,13 +5,13 @@ const PORT = 3000;
 const app = express();
 
 const inventors = [
-    { first: 'Albert', last: 'Einstein', year: 1879 },
-    { first: 'Isaac', last: 'Newton', year: 1643 },
-    { first: 'Galileo', last: 'Galilei', year: 1564 },
-    { first: 'Marie', last: 'Curie', year: 1867 },
-    { first: 'Johannes', last: 'Kepler', year: 1571 },
-    { first: 'Nicolaus', last: 'Copernicus', year: 1473 },
-    { first: 'Max', last: 'Planck', year: 1858 },
+    { _id: 1, first: 'Albert', last: 'Einstein', year: 1879 },
+    { _id: 2, first: 'Isaac', last: 'Newton', year: 1643 },
+    { _id: 3, first: 'Galileo', last: 'Galilei', year: 1564 },
+    { _id: 4, first: 'Marie', last: 'Curie', year: 1867 },
+    { _id: 5, first: 'Johannes', last: 'Kepler', year: 1571 },
+    { _id: 6, first: 'Nicolaus', last: 'Copernicus', year: 1473 },
+    { _id: 7, first: 'Max', last: 'Planck', year: 1858 },
   ];
 
 app.get('/', (req, res) => {
@@ -19,10 +19,29 @@ app.get('/', (req, res) => {
 });
 
 app.get('/api/inventors', (req, res) =>{
+    const order = req.query.order;
+    console.log(order);
     res.json(inventors);
 });
 
+app.get('/api/inventors/:id', (req, res) =>{
+    const id = parseInt(req.params.id);
+    res.json(inventors.filter(inventor => inventor._id === id));
+});
 
+app.use(express.json());
+
+app.post('/api/inventors', (req, res) =>{
+    // const inventorNuevo = {
+    //     _id: 8, first: 'Fernando', last: 'Fernandez', year: 2000
+    // };
+    console.log(req);
+    const inventorNuevo = req.body;
+
+
+    inventorNuevo._id = 8;
+    res.json(inventorNuevo);
+});
 
 app.listen(PORT, () => {
     console.log(chalk.bgGreen.blue(`Server express levantado en el puerto: ${PORT}`));
---
+++ practice/crud-api-inventors.js
@@ -0,0 +1,9 @@
+
+//TODO: Tomando el archivo json inventors
+
+// GET /api/inventors --> retorna todos los inventores
+// GET /api/inventors/:id --> retorna un inventor determinado por ID
+// POST /api/inventors/  --> da de alta un nuevo inventor
+// PUT /api/inventors/:id  --> actualiza las propiedades de un inventor segund ID
+// DELETE /api/inventors/:id  --> elimina un inventor por ID
+
